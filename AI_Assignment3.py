8389427a7577db2e762aa55944af20adf014b27e
MysticMane <140106405+MysticMane@users.noreply.github.com>
2024-01-27 12:31:05 +0530

    Add files via upload

---
+++ AI_Assignment3.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+data= pd.read_csv('mnist_train.csv')
+print(data)
+a = data.iloc[3,1:].values
+a = a.reshape(28,28).astype('uint8')
+print(plt.imshow(a))
+df_x = data.iloc[:,1:]
+df_y = data.iloc[:,0]
+x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size = 0.2, random_state= 4)
+print(x_train.head())
+print(y_train.head())
+rf = RandomForestClassifier(n_estimators=100)
+print(rf.fit(x_train, y_train))
+pred = rf.predict(x_test)
+print(pred)
+s = y_test.values
+count=0
+for i in range(len(pred)):
+    if pred[i]==s[i]:
+        count=count+1
+print(count)
+print(len(pred))
+q=count/len(pred)
+print(q)
